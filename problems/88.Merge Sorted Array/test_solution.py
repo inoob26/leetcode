8878df43da6374feff2f778404d625eb3d534336
inoob26 <inoob26@gmail.com>
2023-03-10 17:41:40 +0600

    merge sorted array

--- problems/88.Merge Sorted Array/test_solution.py
+++ problems/88.Merge Sorted Array/test_solution.py
@@ -10,5 +10,18 @@ def test_solution():
     result = Solution().merge(nums1, m, nums2, n)
     assert expected == result
 
+    nums1 = [1]
+    m = 1
+    nums2 = []
+    n = 0
+    expected = [1]
+    result = Solution().merge(nums1, m, nums2, n)
+    assert expected == result
 
-test_solution()
+    nums1 = [0]
+    m = 0
+    nums2 = [1]
+    n = 1
+    expected = [1]
+    result = Solution().merge(nums1, m, nums2, n)
+    assert expected == result
